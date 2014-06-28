1167f6bcf3feacd514101d78f32bf39e4b7c7902
actors315 <actors315@gmail.com>
2014-06-28 23:59:17 +0800

    添加多数库支持
    将开发，测试，产品所连数据库分割，保证数据安全。

--- service/application/config/database.php
+++ service/application/config/database.php
@@ -38,7 +38,7 @@
 | the active record class
 */
 
-$active_group = 'default';
+$active_group = ENVIRONMENT;
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
@@ -57,6 +57,43 @@ $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
 
+/**
+ * 开发数据库
+ */
+$db['development']['hostname'] = 'localhost';
+$db['development']['username'] = '';
+$db['development']['password'] = '';
+$db['development']['database'] = '';
+$db['development']['dbdriver'] = 'mysql';
+$db['development']['dbprefix'] = '';
+$db['development']['pconnect'] = TRUE;
+$db['development']['db_debug'] = TRUE;
+$db['development']['cache_on'] = FALSE;
+$db['development']['cachedir'] = '';
+$db['development']['char_set'] = 'utf8';
+$db['development']['dbcollat'] = 'utf8_general_ci';
+$db['development']['swap_pre'] = '';
+$db['development']['autoinit'] = TRUE;
+$db['development']['stricton'] = FALSE;
 
+/**
+ * 正式发布版本所用数据库
+ **/
+$db['production']['hostname'] = 'localhost';
+$db['production']['username'] = '';
+$db['production']['password'] = '';
+$db['production']['database'] = '';
+$db['production']['dbdriver'] = 'mysql';
+$db['production']['dbprefix'] = '';
+$db['production']['pconnect'] = TRUE;
+$db['production']['db_debug'] = TRUE;
+$db['production']['cache_on'] = FALSE;
+$db['production']['cachedir'] = '';
+$db['production']['char_set'] = 'utf8';
+$db['production']['dbcollat'] = 'utf8_general_ci';
+$db['production']['swap_pre'] = '';
+$db['production']['autoinit'] = TRUE;
+$db['production']['stricton'] = FALSE;
+ 
 /* End of file database.php */
 /* Location: ./application/config/database.php */
